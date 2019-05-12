eba07dd42a2eac15894e7c989e365892045176e8
Eric Dallo <ericdallo06@hotmail.com>
2019-05-12 12:23:24 -0300

    Add some commands

--- docs/js/terminal-commander.js
+++ docs/js/terminal-commander.js
@@ -1,7 +1,23 @@
 define('terminal-commander', ['doc'], function($) {
 
+    var help = $('#help-text-block').html();
+
+    var commands = {
+        'help': help,
+        'ls': 'about-me.txt  profile.txt  links.txt',
+        'cat': ['about-me.txt', 'profile.txt', 'links.txt']
+    };
+
     var issue = function(text) {
-        return 'bla';
+        if (text === '') {
+            return '';
+        }
+        var command = commands[text];
+        if (command === undefined) {
+            return "Command '" + text + "' not found. In doubt, type 'help'";
+        }
+
+        return command;
     };
 
     return {
