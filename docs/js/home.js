9e863ab43796b650df7229068120feb0cc57310f
Eric Dallo <ericdallo06@hotmail.com>
2019-05-12 11:29:36 -0300

    Add hotkeys

--- docs/js/home.js
+++ docs/js/home.js
@@ -1,15 +1,5 @@
 define(['doc', 'terminal'], function($, $terminal) {
     'use strict';
 
-    $terminal.init();
-
-    $('#input').on('keydown', function(e) {
-        var $this = $(this);
-        if (e.key === 'Enter') {
-            var text = $this.val();
-            $terminal.write(text);
-            
-            $this.val('');
-        }
-    })
+    $terminal.bind();
 });
--- src/js/terminal.js
+++ src/js/terminal.js
@@ -1,28 +1,70 @@
 define('terminal', ['doc'], function($) {
     'use strict';
 
-    var initText                 = $('#initial-text-block').html(),
-        $digitedTextContainer    = $('#digited-text'),
-        $inputContainerParent    = $('#input-container');
+    var buildShellText = function(text) {
+        var $parent    = $(document.createElement('div')),
+            $container = $(document.createElement('section')).addClass('input-container'),
+            $location  = $(document.createElement('span')).addClass('location'),
+            $shell     = $(document.createElement('span')),
+            $text      = $(document.createElement('span')).addClass('static-text');
 
-    var write = function(text) {
-        var $newText = $(document.createElement('pre'));
-        $newText.addClass('text-block');
+        $location.text('~');
+        $shell.text('$');
+        $text.text(text);
+        $container.append($location.first());
+        $container.append($shell.first());
+        $container.append($text.first());
+        $parent.append($container.first());
+        
+        return $parent.html();
+    };
 
-        $newText.html(text);
+    var initTerminal = function() {
+        var initText                 = $('#initial-text-block').html(),
+            $digitedTextContainer    = $('#digited-text'),
+            $input                   = $('#input');
 
-        $digitedTextContainer.append($newText.first());
+        var write = function(text) {
+            var $newText = $(document.createElement('pre'));
+            $newText.addClass('text-block');
+
+            $newText.html(text);
+
+            $digitedTextContainer.append($newText.first());
+        };
+
+        var clear = function() {
+            $digitedTextContainer.html('');
+        };
+
+        $(window).on('keydown', function(e) {
+
+            var text = $input.val();
+        
+            if (e.key === 'Enter') {
+                e.preventDefault();
+                write(buildShellText(text));
+                $input.val('');
+            }
+
+            if (e.keyCode === 76 && e.ctrlKey) {
+                e.preventDefault();
+                clear();
+            }
+
+            if (e.keyCode === 67 && e.ctrlKey) {
+                e.preventDefault();
+                write(buildShellText(text));
+                $input.val('');
+            }
+        });
+
+        write(initText);
     };
 
     return {
-        'write': function(text) {
-            var shell = $inputContainerParent.html();
-
-            write(shell);
-            write(text);
-        },
-        'init': function() {
-            write(initText);
+        'bind': function() {
+            initTerminal();
         }
     };
 });
